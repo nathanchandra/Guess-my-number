140d1e2d85272801982bdc44a410e625d03430f1
nathanchandra <nathanchandra35@gmail.com>
2022-02-02 16:33:45 +1100

    Adding reseting functionlaity

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const secretNum = Math.trunc(Math.random() * 20) + 1;
+let secretNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNum;
+//document.querySelector('.number').textContent = secretNum;
 
 //listening for the click on check button, takes the event we
 //are listening for and a function/method to handle the event
@@ -42,7 +42,22 @@ document.querySelector('.check').addEventListener('click', function () {
     //when the player wins
   } else if (guess === secretNum) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNum;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
+});
+
+//reset functionality
+document.querySelector('.again').addEventListener('click', function () {
+  //restore init conditons of message, number, score and guess input and values
+  score = 20;
+  secretNum = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  //restore background color and number width
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
